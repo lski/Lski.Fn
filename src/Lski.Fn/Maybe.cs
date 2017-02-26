2ce455007384c23f35a4dc92239bbf7e88b7d8a7
lski <lee.cooper@lski.uk>
2017-02-26 11:59:37 +0000

    Added async methods to Maybe functions

--- src/Lski.Fn/Maybe.cs
+++ src/Lski.Fn/Maybe.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Lski.Fn
 {
@@ -38,9 +39,19 @@ namespace Lski.Fn
         public static Result<T> ToResult<T>(this Maybe<T> maybe, string error) => maybe.ToResult(error);
 
         /// <summary>
-        /// Perform the action if Maybe contains a value
+        /// Alias for Bind
         /// </summary>
-        public static Maybe<T> Do<T>(this Maybe<T> maybe, Action<T> action)
+        public static Maybe<T> Do<T>(this Maybe<T> maybe, Action<T> action) => maybe.Bind(action);
+
+        /// <summary>
+        /// Alias for Bind
+        /// </summary>
+        public static Maybe<TOut> Do<T, TOut>(this Maybe<T> maybe, Func<T, Maybe<TOut>> func) => maybe.Bind(func);
+
+        /// <summary>
+        /// Performs the action if Maybe contains a value and returns the same Maybe for chaining.
+        /// </summary>
+        public static Maybe<T> Bind<T>(this Maybe<T> maybe, Action<T> action)
         {
             if (maybe.HasValue)
             {
@@ -51,13 +62,106 @@ namespace Lski.Fn
         }
 
         /// <summary>
-        /// Alias for Bind
-        /// </summary>
-        public static Maybe<TOut> Do<T, TOut>(this Maybe<T> maybe, Func<T, Maybe<TOut>> func) => maybe.Bind(func);
-
-        /// <summary>
-        /// If contains a value performs the function and returns a new Maybe, otherwise returns a "none" Maybe
+        /// If contains a value performs the function and returns a new Maybe, otherwise returns a new Maybe.None
         /// </summary>
         public static Maybe<TOut> Bind<T, TOut>(this Maybe<T> maybe, Func<T, Maybe<TOut>> func) => maybe.HasValue ? func(maybe.Value) : Maybe<TOut>.None();
+
+        /// <summary>
+        /// Returns either the stored value or the default if contains nothing
+        /// </summary>
+        public static async Task<T> Unwrap<T>(this Task<Maybe<T>> task, T defaultValue = default(T))
+        {
+            var maybe = await task.ConfigureAwait(false);
+            return maybe.Unwrap(defaultValue);
+        }
+
+        /// <summary>
+        /// Convert the maybe into a result, a value means success, nothing means failure
+        /// </summary>
+        public static async Task<Result<T>> ToResult<T>(this Task<Maybe<T>> task, Error error)
+        {
+            var maybe = await task.ConfigureAwait(false);
+            return maybe.ToResult(error);
+        }
+
+        /// <summary>
+        /// Convert the maybe into a result, a value means success, nothing means failure
+        /// </summary>
+        public static async Task<Result<T>> ToResult<T>(this Task<Maybe<T>> task, string error)
+        {
+            var maybe = await task.ConfigureAwait(false);
+            return maybe.ToResult(error);
+        }
+
+        /// <summary>
+        /// Alias for Bind
+        /// </summary>
+        public static async Task<Maybe<T>> Do<T>(this Task<Maybe<T>> maybe, Action<T> action) => await maybe.Bind(action);
+
+        /// <summary>
+        /// Alias for Bind
+        /// </summary>
+        public static async Task<Maybe<TOut>> Do<T, TOut>(this Task<Maybe<T>> maybe, Func<T, Maybe<TOut>> func) => await maybe.Bind(func);
+
+        /// <summary>
+        /// Performs the action if Maybe contains a value and returns the same Maybe for chaining.
+        /// </summary>
+        public static async Task<Maybe<T>> Bind<T>(this Task<Maybe<T>> task, Action<T> action)
+        {
+            var maybe = await task.ConfigureAwait(false);
+            if (maybe.HasValue)
+            {
+                action(maybe.Value);
+            }
+
+            return maybe;
+        }
+
+        /// <summary>
+        /// If contains a value performs the function and returns a new Maybe, otherwise returns a new Maybe.None
+        /// </summary>
+        public static async Task<Maybe<TOut>> Bind<T, TOut>(this Task<Maybe<T>> task, Func<T, Maybe<TOut>> func)
+        {
+            var maybe = await task.ConfigureAwait(false);
+            return maybe.HasValue ? func(maybe.Value) : Maybe<TOut>.None();
+        }
+
+        /// <summary>
+        /// Alias for Bind
+        /// </summary>
+        public static async Task<Maybe<TOut>> Do<T, TOut>(this Maybe<T> maybe, Func<T, Task<Maybe<TOut>>> func) => await maybe.Bind(func);
+
+        /// <summary>
+        /// If contains a value performs the function and returns a new Maybe, otherwise returns a new Maybe.None
+        /// </summary>
+        public static async Task<Maybe<TOut>> Bind<T, TOut>(this Maybe<T> maybe, Func<T, Task<Maybe<TOut>>> func)
+        {
+            if (maybe.HasValue)
+            {
+                return await func(maybe.Value);
+            }
+
+            return Maybe<TOut>.None();
+        }
+
+        /// <summary>
+        /// Alias for Bind
+        /// </summary>
+        public static async Task<Maybe<TOut>> Do<T, TOut>(this Task<Maybe<T>> maybe, Func<T, Task<Maybe<TOut>>> func) => await maybe.Bind(func);
+
+        /// <summary>
+        /// If contains a value performs the function and returns a new Maybe, otherwise returns a new Maybe.None
+        /// </summary>
+        public static async Task<Maybe<TOut>> Bind<T, TOut>(this Task<Maybe<T>> task, Func<T, Task<Maybe<TOut>>> func)
+        {
+            var maybe = await task.ConfigureAwait(false);
+
+            if (maybe.HasValue)
+            {
+                return await func(maybe.Value);
+            }
+
+            return Maybe<TOut>.None();
+        }
     }
 }

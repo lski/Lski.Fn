a5bf9067f7832d8c48e531d52e4255c44bbbc00a
Lee Cooper <Lee Cooper>
2017-03-13 19:26:28 +0000

    Added Ensure function to Result

--- src/Lski.Fn/ResultExtensions.cs
+++ src/Lski.Fn/ResultExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 
 namespace Lski.Fn
@@ -25,7 +26,6 @@ namespace Lski.Fn
         [DebuggerStepThrough]
         public static Result<T> ToFail<T>(this Error message) => Result.Fail<T>(message);
 
-
         /// <summary>
         /// Turn an error message to a failed result
         /// </summary>
@@ -38,6 +38,32 @@ namespace Lski.Fn
         [DebuggerStepThrough]
         public static Result<T> ToFail<T>(this string message) => Result.Fail<T>(message);
 
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static Result Ensure(this Result result, Func<bool> check, Error error)
+        {
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail();
+            }
 
+            return check() ? Result.Success() : error.ToFail();
+        }
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static Result<T> Ensure<T>(this Result<T> result, Func<T, bool> check, Error error)
+        {
+            if (result.IsFailure)
+            {
+                return result;
+            }
+
+            return check(result.Value) ? result.Value.ToSuccess() : error.ToFail<T>();
+        }
     }
 }
--- src/Lski.Fn/ResultExtensionsAsync.cs
+++ src/Lski.Fn/ResultExtensionsAsync.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -56,6 +57,108 @@ namespace Lski.Fn
         {
             var error = await task.ConfigureAwait(false);
             return Result.Fail<T>(error);
+        }
+
+
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static async Task<Result> Ensure(this Task<Result> task, Func<bool> check, Error error)
+        {
+            var result = await task.ConfigureAwait(false);
+
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail();
+            }
+
+            return check() ? Result.Success() : error.ToFail();
+        }
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static async Task<Result<T>> Ensure<T>(this Task<Result<T>> task, Func<T, bool> check, Error error)
+        {
+            var result = await task.ConfigureAwait(false);
+
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail<T>();
+            }
+
+            return check(result.Value) ? result.Value.ToSuccess() : error.ToFail<T>();
+        }
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static async Task<Result> Ensure(this Result result, Func<Task<bool>> check, Error error)
+        {
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail();
+            }
+
+            var passed = await check().ConfigureAwait(false);
+
+            return passed ? Result.Success() : Result.Fail(error);
+        }
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static async Task<Result<T>> Ensure<T>(this Result<T> result, Func<T, Task<bool>> check, Error error)
+        {
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail<T>();
+            }
+
+            var passed = await check(result.Value).ConfigureAwait(false);
+
+            return passed ? Result.Success(result.Value) : Result.Fail<T>(error);
+        }
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static async Task<Result> Ensure(this Task<Result> task, Func<Task<bool>> check, Error error)
+        {
+            var result = await task.ConfigureAwait(false);
+
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail();
+            }
+
+            var passed = await check().ConfigureAwait(false);
+
+            return passed ? Result.Success() : Result.Fail(error);
+        }
+
+        /// <summary>
+        /// Runs the check function if result is a success. If check returns false the a Failed result is returned with the error message
+        /// </summary>
+        [DebuggerStepThrough]
+        public static async Task<Result<T>> Ensure<T>(this Task<Result<T>> task, Func<T, Task<bool>> check, Error error)
+        {
+            var result = await task.ConfigureAwait(false);
+
+            if (result.IsFailure)
+            {
+                return result.Error.ToFail<T>();
+            }
+
+            var passed = await check(result.Value).ConfigureAwait(false);
+
+            return passed ? Result.Success(result.Value) : Result.Fail<T>(error);
         }
     }
 }
